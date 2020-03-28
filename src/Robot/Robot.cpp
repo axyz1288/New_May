92d3d581f69c65c8c850ef3a22c6f21fa519d45e
Eric_Chang <axyz1350@gmail.com>
2020-03-28 04:27:48 +0000

    update CMakefile

--- src/Robot/Robot.cpp
+++ src/Robot/Robot.cpp
@@ -1,28 +1,28 @@
 #include "Robot.h"
 Robot *Robot::inst_ = nullptr;
 Robot *CRobot = Robot::getRobot();
+/* Speech */
+Speech *CSpeech = CRobot->CSpeech;
+
+/* Vision */
+Vision *CVision = CRobot->CVision;
+
+/* Body */
+Body *CBody = CRobot->CBody;
+// Head and Lifting
+HeadandLiftingPlatform *CHeadandLifting = CRobot->CBody->CHeadandLifting;
+// Arm
+SaleArmLeft *CLeftArm = CRobot->CBody->CSaleArmLeft;
+SaleArmRight *CRightArm = CRobot->CBody->CSaleArmRight;
+// MobilePlatform
+MobilePlatform *CMobilePlatform = CRobot->CBody->CMobilePlatform;
+Steering *CSteering = CRobot->CBody->CMobilePlatform->CSteering;
+Wheel *CWheel = CRobot->CBody->CMobilePlatform->CWheel;
 
 Robot *Robot::getRobot()
 {
     if (inst_ == nullptr)
         inst_ = new Robot();
-    /* Speech */
-    Speech *CSpeech = CRobot->CSpeech;
-
-    /* Vision */
-    Vision *CVision = CRobot->CVision;
-
-    /* Body */
-    Body *CBody = CRobot->CBody;
-    // Head and Lifting
-    HeadandLiftingPlatform *CHeadandLifting = CRobot->CBody->CHeadandLifting;
-    // Arm
-    SaleArmLeft *CLeftArm = CRobot->CBody->CSaleArmLeft;
-    SaleArmRight *CRightArm = CRobot->CBody->CSaleArmRight;
-    // MobilePlatform
-    CMobilePlatform = CRobot->CBody->CMobilePlatform;
-    CSteering = CRobot->CBody->CMobilePlatform->CSteering;
-    CWheel = CRobot->CBody->CMobilePlatform->CWheel;
     return inst_;
 }
 
